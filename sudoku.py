e8926c855600eeb9b6234ce5992cf0c19e8197f3
Gabriel Carmona <gabrielrache123@hotmail.com>
2024-01-25 23:04:02 -0300

    fixed issue #1

--- sudoku.py
+++ sudoku.py
@@ -2,25 +2,68 @@ import numpy as np
 
 # https://www.sudokuwiki.org/Getting_Started
 
-def TestaPossibilidade (jogo, l, c, caixa):
+
+### To Do List: 
+
+## Basic Strategies
+# Pinned!
+# Naked Single
+# Naked Pairs
+# Naked Triples
+# Naked Quads
+# Hidden Candidates
+# Intersection Removal
+## Tough Strategies
+# X-Wing
+# Singles Chains
+# Y-Wing
+# Rectangle Elimination
+# Swordfish
+# XYZ-Wing
+# BUG - Bi-Value Universal Grave
+# Avoidable Rectangles
+
+
+# Done!
+
+# Last Remaining Cell in a Box
+# Last Remaining Cell in a Row (or Column)
+
+
+def TestaPossibilidade (jogo, l, c, caixaMenardo):
 
     possibilidade  = np.array([1,2,3,4,5,6,7,8,9]) 
 
     naoPossibilidade = [] 
 
-    quadro = RetornaQuadro(jogo,caixa)
+    caixa = RetornaQuadro(jogo,caixaMenardo)
     
     # Method: Last Remaining Cell in a Row (or Column)
+    naoPossibilidade = M_Last_Remaining_Cell_Row (jogo, l, c)
+    possibilidade = (list(set(possibilidade).difference(naoPossibilidade)))
+
+    # Method: Last Remaining Cell in a Box
+    naoPossibilidade = M_Last_Remaining_Cell_Box (caixa)
+    possibilidade = (list(set(possibilidade).difference(naoPossibilidade)))
+
+    return (list(set(possibilidade).difference(naoPossibilidade)))
+
+
+def M_Last_Remaining_Cell_Row (jogo, l, c):
+    naoPossibilidade = []
     for x in range (0,9):
         naoPossibilidade.append(jogo[l,x])
         naoPossibilidade.append(jogo[x,c])
+    return naoPossibilidade
 
-    # Method: Last Remaining Cell in a Box
-    for y in range (0,3):
-        naoPossibilidade.append(quadro[l%3,y])
-        naoPossibilidade.append(quadro[y,c%3])
+def M_Last_Remaining_Cell_Box (caixa):
+    naoPossibilidade = []
+    for l in range (3):
+        for c in range (3):
+            naoPossibilidade.append(caixa[l,c])
+    print (naoPossibilidade)
+    return naoPossibilidade
 
-    return (list(set(possibilidade).difference(naoPossibilidade)))
 
 def RetornaQuadro (jogo, caixa):
     dict = {
